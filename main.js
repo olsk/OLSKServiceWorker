b908372d9719768d1259ab395b7e4ba1330177ee
Rosano Coutinho <pub@xeari.com>
2020-08-14 17:34:32 -0300

    Purge _OLSKServiceWorkerTemplate

--- main.js
+++ main.js
@@ -12,64 +12,6 @@ const mod = {
 		return !!inputData.url.match(/^https\:\/\/rosano\.ca\/api/);
 	},
 
-	_OLSKServiceWorkerTemplate () {
-		const cacheName = 'OLSKServiceWorkerCache-VERSION_ID_TOKEN';
-
-		self.addEventListener('activate', async function (event) {
-			console.log('activate', event);
-			
-			event.waitUntil(Promise.all(
-				(await caches.keys()).map(function (e) {
-					return caches.delete(e);
-				})
-			));
-		});
-
-		self.addEventListener('fetch', function (event) {
-			if (event.request.method !== 'GET') {
-				return console.log('ignoring non-Get', event.request);
-			}
-
-			if (event.request.url.match('sw.js')) {
-				return console.log('ignoring sw.js', event.request);
-			}
-
-			if (event.request.mode === 'cors') {
-				return console.log('ignoring cors', event.request);
-			}
-
-			if (!(event.request.referrer.match(/ORIGIN_PAGE_PATH_TOKEN/) && event.request.mode === 'no-cors') && !event.request.url.match(/ORIGIN_PAGE_PATH_TOKEN/)) {
-				return console.log('ignoring referrer', event.request);
-			};
-
-			event.respondWith(async function() {
-				let cacheResponse = await caches.match(event.request);
-
-				if (cacheResponse) {
-					return cacheResponse;
-				}
-
-				let networkResponse = await fetch(event.request);
-
-				if (networkResponse.status !== 200) {
-					return networkResponse;
-				}
-
-				(await caches.open(cacheName)).put(event.request, networkResponse.clone());
-
-				return networkResponse;
-			}());
-		});
-
-		self.addEventListener('message', function (event) {
-			console.log('message', event);
-
-		  if (event.data.action === 'skipWaiting') {
-		    self.skipWaiting();
-		  }
-		});
-	},
-
 	OLSKServiceWorkerModule (param1, param2, param3, param4) {
 		if (typeof param1 !== 'object' || param1 === null) {
 			throw new Error('OLSKErrorInputNotValid');
