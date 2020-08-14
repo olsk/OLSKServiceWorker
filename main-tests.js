21d5e8d3e12f7a588106cac602fa42c02479234e
Rosano Coutinho <pub@xeari.com>
2020-08-14 10:36:58 -0300

    Spec OLSKServiceWorkerViewTemplate

--- main-tests.js
+++ main-tests.js
@@ -9,6 +9,94 @@ const uStubTokens = function () {
 	}
 };
 
+const uFakeSelf = function() {
+	const _listeners = {};
+	const _calls = {};
+
+	return {
+		addEventListener (param1, param2) {
+			_listeners[param1] = param2;
+		},
+		skipWaiting () {
+			_calls.skipWaiting = (_calls.skipWaiting || 0) + 1;
+		},
+		_FakeListeners () {
+			return _listeners;
+		},
+		_FakeCalls () {
+			return _calls;
+		},
+	};
+};
+
+const uFakeCaches = function(inputData = []) {
+	const _calls = {};
+	const _deletes = [];
+	const _caches = {};
+
+	return {
+		keys () {
+			_calls.keys = (_calls.keys || 0) + 1;
+
+			return inputData;
+		},
+		delete (inputData) {
+			_deletes.push(inputData);
+		},
+		open (cacheName) {
+			_caches[cacheName] = {};
+
+			return {
+				put (request, value) {
+					_caches[cacheName][request.url] = value;
+				},
+			};
+		},
+		async match (inputData) {
+			return Object.values(_caches).reduce(function (coll, item) {
+				if (Object.keys(item).includes(inputData.url)) {
+					coll.push(item[inputData.url]);
+				}
+
+				return coll;
+			}, []).shift();
+		},
+		_FakeCalls () {
+			return _calls;
+		},
+		_FakeDeletes () {
+			return _deletes;
+		},
+		_FakeCaches () {
+			return _caches;
+		},
+	};
+};
+
+const uFakeFetch = async function (inputData) {
+	this._FakeRequests = [];
+	this._FakeRequests.push(inputData.url);
+
+	this._FakeResponses = [];
+	this._FakeResponses.push({
+		url: inputData.url,
+		status: 200,
+		clone () {
+			return Object.assign({}, this);
+		},
+	});
+
+	return this._FakeResponses.slice(-1).pop();
+};
+
+const uData = function(inputData) {
+	return inputData + '-DATA';
+};
+
+const uModule = function(param1 = uFakeSelf()) {
+	return mainModule.OLSKServiceWorkerModule(param1, uFakeCaches(), uFakeFetch);
+};
+
 describe('OLSKServiceWorkerRequestMatchesROCOAPI', function test_OLSKServiceWorkerRequestMatchesROCOAPI() {
 
 	const uRequest = function (inputData) {
@@ -91,6 +179,317 @@ describe('OLSKServiceWorkerView', function test_OLSKServiceWorkerView() {
 
 	it('returns function body', function() {
 		deepEqual(mainModule.OLSKServiceWorkerView(uStubTokens()), mainModule._OLSKServiceWorkerTemplate.toString().replace('VERSION_ID_TOKEN', 'alfa').replace(/REFERRER_MATCH_TOKEN/g, 'bravo').replace('_OLSKServiceWorkerTemplate () {', '').trim().slice(0, -1));
+	});
+
+});
+
+describe('OLSKServiceWorkerModule', function test_OLSKServiceWorkerModule() {
+
+	it('throws if param1 not object', function() {
+		throws(function() {
+			mainModule.OLSKServiceWorkerModule(null, uFakeCaches());
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param1 not valid', function() {
+		throws(function() {
+			mainModule.OLSKServiceWorkerModule({
+				addEventListener: {},
+			}, uFakeCaches());
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not object', function() {
+		throws(function() {
+			mainModule.OLSKServiceWorkerModule(uFakeSelf(), null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not valid', function() {
+		throws(function() {
+			mainModule.OLSKServiceWorkerModule(uFakeSelf(), {
+				keys: {},
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param3 not function', function() {
+		throws(function() {
+			mainModule.OLSKServiceWorkerModule(uFakeSelf(), uFakeCaches(), null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns object', function() {
+		deepEqual(typeof uModule(), 'object');
+	});
+
+	context('ControlClearCache', function () {
+		
+		it('calls caches.delete on caches.all', async function () {
+			const item = uFakeCaches(['alfa', 'bravo']);
+
+			await Object.assign(uModule(), {
+				_ValueCaches: item,
+			}).ControlClearCache();
+
+			deepEqual(item._FakeCalls().keys, 1);
+			deepEqual(item._FakeDeletes(), ['alfa', 'bravo']);
+		});
+	
+	});
+
+	context('OLSKServiceWorkerDidActivate', function () {
+		
+		it('calls event.waitUntil with mod.ControlClearCache()', function () {
+			const item = {};
+
+			const mod = Object.assign(uModule(), {
+				ControlClearCache () {
+					return 'alfa';
+				},
+			});
+			
+			mod.OLSKServiceWorkerDidActivate({
+				waitUntil (inputData) {
+					item.waitUntil = inputData;
+				},
+			});
+
+			deepEqual(item.waitUntil, 'alfa');
+		});
+
+	});
+
+	context('OLSKServiceWorkerDidFetch', function () {
+
+		const uFetchEvent = function (inputData) {
+			const _FakeResponses = [];
+
+			return {
+				async respondWith (inputData) {
+					return _FakeResponses.push(await inputData);
+				},
+				request: Object.assign({
+					method: 'GET',
+					url: 'https://alfa.bravo/',
+					mode: 'cors',
+					referrer: 'https://charlie.delta/',
+				}, inputData),
+				_FakeResponses () {
+					return _FakeResponses;
+				},
+			};
+		};
+
+		it('ignores if method POST', async function () {
+			const item = uFetchEvent({
+				method: 'POST',
+			});
+
+			await uModule().OLSKServiceWorkerDidFetch(item);
+
+			deepEqual(item._FakeResponses(), []);
+		});
+
+		it('ignores if url sw.js', async function () {
+			const item = uFetchEvent({
+				url: 'https://alfa.bravo/sw.js',
+			});
+
+			await uModule().OLSKServiceWorkerDidFetch(item);
+
+			deepEqual(item._FakeResponses(), []);
+		});
+
+		context('response', function () {
+
+			const uObject = function (key, value) {
+				const outputData = {};
+
+				outputData[key] = value;
+
+				return outputData;
+			};
+
+			context('network_not_ok', function () {
+
+				const event = uFetchEvent();
+				const mod = Object.assign(uModule(), {
+					_alfa: uFakeFetch,
+					async _ValueFetch (inputData) {
+						return Object.assign(await mod._alfa(inputData), {
+							status: 401,
+						});
+					},
+				});
+
+				before(function () {
+					return mod.OLSKServiceWorkerDidFetch(event);
+				});
+
+				it('requests fetch', function () {
+					deepEqual(mod._FakeRequests, [event.request.url]);
+				});
+
+				it('returns network response', function () {
+					deepEqual(mod._FakeResponses, event._FakeResponses());
+				});
+
+				it('caches no response', function () {
+					deepEqual(mod._ValueCaches._FakeCaches(), {});
+				});
+			
+			});
+
+			context('network_ok', function () {
+
+				const event = uFetchEvent();
+				const mod = uModule();
+
+				before(function () {
+					return mod.OLSKServiceWorkerDidFetch(event);
+				});
+
+				it('performs network fetch', function () {
+					deepEqual(mod._FakeRequests, [event.request.url]);
+				});
+
+				it('returns network response', function () {
+					deepEqual(mod._FakeResponses, event._FakeResponses());
+				});
+
+				it('caches response', function () {
+					deepEqual(mod._ValueCaches._FakeCaches(), uObject(mod._DataCacheName, uObject(event.request.url, mod._FakeResponses[0])));
+				});
+			
+			});
+
+			context('cached', function () {
+
+				const event = uFetchEvent();
+				const mod = uModule();
+
+				before(function () {
+					return mod.OLSKServiceWorkerDidFetch(event);
+				});
+
+				before(function () {
+					return mod.OLSKServiceWorkerDidFetch(event);
+				});
+
+				it('performs no network fetch', function () {
+					deepEqual(mod._FakeRequests, [event.request.url]);
+				});
+
+				it('returns cached response', function () {
+					deepEqual(event._FakeResponses(), [mod._FakeResponses[0], mod._FakeResponses[0]]);
+				});
+
+			});
+		
+		});
+	
+	});
+
+	context('OLSKServiceWorkerDidReceiveMessage', function () {
+		
+		it('calls skipWaiting if data.action skipWaiting', function () {
+			const item = uFakeSelf();
+			const mod = uModule(item);
+			
+			mod.OLSKServiceWorkerDidReceiveMessage({
+				data: {
+					action: 'skipWaiting',
+				},
+			});
+
+			deepEqual(item._FakeCalls().skipWaiting, 1);
+		});
+
+		it('does nothing', function () {
+			const item = uFakeSelf();
+			const mod = uModule(item);
+			
+			mod.OLSKServiceWorkerDidReceiveMessage({
+				data: {
+					action: 'alfa',
+				},
+			});
+
+			deepEqual(item._FakeCalls().skipWaiting, undefined);
+		});
+	
+	});
+
+});
+
+describe('OLSKServiceWorkerInitialization', function test_OLSKServiceWorkerInitialization() {
+
+	it('throws if param1 not object', function() {
+		throws(function() {
+			mainModule.OLSKServiceWorkerInitialization(null, uModule());
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param1 not valid', function() {
+		throws(function() {
+			mainModule.OLSKServiceWorkerInitialization({
+				addEventListener: {},
+			}, uModule());
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not object', function() {
+		throws(function() {
+			mainModule.OLSKServiceWorkerInitialization(uFakeSelf(), null);
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('throws if param2 not valid', function() {
+		throws(function() {
+			mainModule.OLSKServiceWorkerInitialization(uFakeSelf(), {
+				OLSKServiceWorkerDidReceiveMessage: {},
+			});
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns undefined', function() {
+		deepEqual(mainModule.OLSKServiceWorkerInitialization(uFakeSelf(), uModule()), undefined);
+	});
+
+	it('calls addEventListener for activate', function () {
+		const item = uFakeSelf();
+		const mod = uModule(item);
+		
+		mainModule.OLSKServiceWorkerInitialization(item, mod);
+
+		deepEqual(item._FakeListeners().activate, mod.OLSKServiceWorkerDidActivate);
+	});
+
+	it('calls addEventListener for fetch', function () {
+		const item = uFakeSelf();
+		const mod = uModule(item);
+		
+		mainModule.OLSKServiceWorkerInitialization(item, mod);
+
+		deepEqual(item._FakeListeners().fetch, mod.OLSKServiceWorkerDidFetch);
+	});
+
+	it('calls addEventListener for message', function () {
+		const item = uFakeSelf();
+		const mod = uModule(item);
+		
+		mainModule.OLSKServiceWorkerInitialization(item, mod);
+
+		deepEqual(item._FakeListeners().message, mod.OLSKServiceWorkerDidReceiveMessage);
+	});
+
+});
+
+describe('OLSKServiceWorkerViewTemplate', function test_OLSKServiceWorkerViewTemplate() {
+
+	it('returns string', function() {
+		deepEqual(mainModule.OLSKServiceWorkerViewTemplate(), `const mod = (function ${ mainModule.OLSKServiceWorkerModule.toString() })();\n\n(function ${ mainModule.OLSKServiceWorkerInitialization.toString() })(self, mod);`);
 	});
 
 });
--- main.js
+++ main.js
@@ -102,5 +102,125 @@
 
 		return mod._OLSKServiceWorkerTemplate.toString().replace(/VERSION_ID_TOKEN/g, inputData.VERSION_ID_TOKEN).replace(/REFERRER_MATCH_TOKEN/g, inputData.REFERRER_MATCH_TOKEN).replace('_OLSKServiceWorkerTemplate () {', '').trim().slice(0, -1);
 	},
+
+	OLSKServiceWorkerModule (param1, param2, param3) {
+		if (typeof param1 !== 'object' || param1 === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param1.addEventListener !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'object' || param2 === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2.keys !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param3 !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		const mod = {
+
+			// VALUE
+
+			_ValueSelf: param1,
+			_ValueCaches: param2,
+			_ValueFetch: param3,
+
+			// DATA
+
+			_DataCacheName: 'OLSKServiceWorkerCache-VERSION_ID_TOKEN',
+
+			// CONTROL
+
+			async ControlClearCache () {
+				return Promise.all(
+					(await mod._ValueCaches.keys()).map(function (e) {
+						return mod._ValueCaches.delete(e);
+					})
+				);
+			},
+
+			// MESSAGE
+
+			OLSKServiceWorkerDidActivate (event) {
+				event.waitUntil(mod.ControlClearCache());
+			},
+
+			async OLSKServiceWorkerDidFetch (event) {
+				if (event.request.method !== 'GET') {
+					return;
+				}
+
+				if (event.request.url.match('sw.js')) {
+					return;
+				}
+
+				// if (event.request.mode === 'cors') {
+				// 	return console.log('ignoring cors', event.request);
+				// }
+
+				// if (!(event.request.referrer.match(/REFERRER_MATCH_TOKEN/) && event.request.mode === 'no-cors') && !event.request.url.match(/REFERRER_MATCH_TOKEN/)) {
+				// 	return console.log('ignoring referrer', event.request);
+				// };
+
+				return event.respondWith(async function() {
+					let cacheResponse = await mod._ValueCaches.match(event.request);
+
+					if (cacheResponse) {
+						return cacheResponse;
+					}
+
+					let networkResponse = await mod._ValueFetch(event.request);
+
+					if (networkResponse.status === 200) {
+						(await mod._ValueCaches.open(mod._DataCacheName)).put(event.request, networkResponse.clone());
+					}
+
+					return networkResponse;
+				}());
+			},
+
+			OLSKServiceWorkerDidReceiveMessage (event) {
+				if (event.data.action === 'skipWaiting') {
+				  mod._ValueSelf.skipWaiting();
+				}
+			},
+		
+		};
+		
+		return mod;
+	},
+
+	OLSKServiceWorkerInitialization (param1, param2) {
+		if (typeof param1 !== 'object' || param1 === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param1.addEventListener !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'object' || param2 === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2.OLSKServiceWorkerDidReceiveMessage !== 'function') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		param1.addEventListener('activate', param2.OLSKServiceWorkerDidActivate);
+		param1.addEventListener('fetch', param2.OLSKServiceWorkerDidFetch);
+		param1.addEventListener('message', param2.OLSKServiceWorkerDidReceiveMessage);
+	},
+
+	OLSKServiceWorkerViewTemplate () {
+		return `const mod = (function ${ mod.OLSKServiceWorkerModule.toString() })();\n\n(function ${ mod.OLSKServiceWorkerInitialization.toString() })(self, mod);`;
+	},
 	
 }); })));
