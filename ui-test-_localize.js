32c4e1b1d93979790840d58d049c81256cf1ebe3
Rosano Coutinho <pub@xeari.com>
2021-04-28 10:25:06 -0300

    Tidy tests

--- ui-test-_localize.js
+++ ui-test-_localize.js
@@ -6,7 +6,7 @@ kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
 		return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
 	};
 
-	describe(`OLSKServiceWorker_Localize-${ OLSKRoutingLanguage }`, function () {
+	describe('OLSKServiceWorker_Localize-' + OLSKRoutingLanguage, function () {
 
 		before(function() {
 			return browser.OLSKVisit(kDefaultRoute, {
